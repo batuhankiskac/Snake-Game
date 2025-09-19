f690b0f5a78ec407013e3cd18ea23befce4cd64d
batuhankiskac <kiskacbatuhan@gmail.com>
2025-09-19 10:31:08 +0300

    Refactor main function to improve readability and structure

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import pygame
 
 def main():
 	pygame.init()
-	pygame.display.set_mode((800, 800))
+	surface = pygame.display.set_mode((800, 800))
+	pygame.display.set_caption("PySnake")
+	color = (255, 0, 0)
+
+	clock = pygame.time.Clock()
 
 	running = True
-
 	while running:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				running = False
-				print("Exited")
+		surface.fill(color)
+		pygame.display.flip()
+		clock.tick(10)
 
 if __name__ == "__main__":
 	main()
